00a691a2c1e0ff5e37e05a6e68bd893c58710b49
Parakh Jain <parakhjain@Parakhs-iMac.local>
2024-05-30 15:20:14 +0530

    fix width issue

--- src/pages/interface/components/Interface-Chatbot/Message.tsx
+++ src/pages/interface/components/Interface-Chatbot/Message.tsx
@@ -11,16 +11,6 @@ function Message({ message, isJSONString, dragRef }) {
   return (
     <Box className="w-100">
       {message?.role === "user" ? (
-        // <Box className="chat-row flex w-100 box-sizing-border-box mr-2">
-        // <Box className="chat-user-container">
-        //   <Box className="chat-row-user">
-        //     <Typography variant="body1" className="ml-2">
-        //       {message?.content}
-        //     </Typography>
-        //   </Box>
-        //   <AccountBoxIcon className="botIcon" />
-        // </Box>
-
         <Stack
           sx={{
             alignItems: "flex-end",
@@ -72,47 +62,6 @@ function Message({ message, isJSONString, dragRef }) {
             }}
             spacing="5px"
           > */}
-          {/* <Avatar
-              src="https://objectstorage.me-dubai-1.oraclecloud.com/n/axwzijd5v1vn/b/DSL_IMAGES/o/IMAGE/aaec5f35-44d0-4230-a9e6-bb127f2ff6eb-profile-picture-smiling-successful-young-260nw-2040223583.png"
-              sx={{
-                borderRadius: 'calc(16px*1.5)',
-                boxShadow: '0px 0px calc(16px*1.5) gray',
-                fontFamily: 'var(--theme-font-family)',
-                height: '50px',
-                width: '50px',
-                objectFit: 'cover',
-                '&.MuiAvatar-root': { boxShadow: 'none' },
-                ' @media(max-width:991px)': {
-                  height: '35px',
-                  width: '35px',
-                },
-                ' @media(max-width:479px)': {
-                  height: '25px',
-                  width: '25px',
-                },
-              }}
-              alt=""></Avatar> */}
-
-          {/* <AccountBoxIcon
-              sx={{
-                borderRadius: "calc(16px*1.5)",
-                // boxShadow: '0px 0px calc(16px*1.5) gray',
-                // fontFamily: 'var(--theme-font-family)',
-                height: "30px",
-                width: "30px",
-                objectFit: "cover",
-                "&.MuiAvatar-root": { boxShadow: "none" },
-                " @media(max-width:991px)": {
-                  height: "25px",
-                  width: "25px",
-                },
-                " @media(max-width:479px)": {
-                  height: "25px",
-                  width: "25px",
-                },
-              }}
-            />
-             */}
 
           {/* <Typography
               variant="p"
@@ -128,58 +77,11 @@ function Message({ message, isJSONString, dragRef }) {
           {/* </Stack> */}
         </Stack>
       ) : (
-        // </Box>
-        // <Box className="chat-row w-100 box-sizing-border-box mr-2">
-        //   <Box className="w-100 flex-start-start">
-        //     <SmartToyIcon className="mr-1" />
-        //     {message?.wait ? (
-        //       <Box className="flex-start-center w-100 gap-2 p-1 ml-3">
-        //         <div className="loader" />
-        //         <Typography variant="body">{message?.content}</Typography>
-        //       </Box>
-        //     ) : message?.timeOut ? (
-        //       <Box className="flex-start-center w-100 gap-5 p-1">
-        //         <Typography variant="body">
-        //           Timeout reached. Please try again later.
-        //         </Typography>
-        //       </Box>
-        //     ) : (
-        //       <Box className="w-100 flex-start-center">
-        //         {isJSONString(message?.content || "{}")?.responseId ? (
-        //           <InterfaceGrid
-        //             style={{ height: window.innerHeight }}
-        //             dragRef={dragRef}
-        //             inpreview={false}
-        //             ingrid={false}
-        //             gridId={
-        //               JSON.parse(message?.content || "{}")?.responseId ||
-        //               "default"
-        //             }
-        //             loadInterface={false}
-        //             componentJson={JSON.parse(message?.content || "{}")}
-        //             msgId={message?.createdAt}
-        //           />
-        //         ) : (
-        //           <Typography className="ml-1 flex-start-center">
-        //             {message?.content}
-        //             <ReportProblemIcon
-        //               fontSize="small"
-        //               color="error"
-        //               className="ml-2"
-        //             />
-        //           </Typography>
-        //         )}
-        //       </Box>
-        //     )}
-        //   </Box>
-        // </Box>
         <Stack
           sx={{
             alignItems: "flex-end",
             gap: "10px",
-            // width: "100%",
             maxWidth: "90%",
-            // minHeight: "50px",
             " @media(max-width:479px)": {
               // height: "90px",
               height: "fit-content",
@@ -198,45 +100,6 @@ function Message({ message, isJSONString, dragRef }) {
             }}
             spacing="5px"
           >
-            {/* <Avatar
-              src="https://objectstorage.me-dubai-1.oraclecloud.com/n/axwzijd5v1vn/b/DSL_IMAGES/o/IMAGE/0c1606a6-23b7-4817-82c1-39da53650fec-round_profil_picture_before_ (1).png"
-              sx={{
-                borderRadius: 'calc(16px*1.5)',
-                boxShadow: '0px 0px calc(16px*1.5) gray',
-                fontFamily: 'var(--theme-font-family)',
-                height: '50px',
-                width: '50px',
-                objectFit: 'cover',
-                '&.MuiAvatar-root': { boxShadow: 'none' },
-                ' @media(max-width:991px)': {
-                  height: '35px',
-                  width: '35px',
-                },
-                ' @media(max-width:479px)': {
-                  height: '25px',
-                  width: '25px',
-                },
-              }}
-              alt=""></Avatar> */}
-            {/* <SmartToyIcon
-              sx={{
-                borderRadius: "calc(16px*1.5)",
-                // boxShadow: '0px 0px calc(16px*1.5) gray',
-                // fontFamily: 'var(--theme-font-family)',
-                height: "30px",
-                width: "30px",
-                objectFit: "cover",
-                "&.MuiAvatar-root": { boxShadow: "none" },
-                " @media(max-width:991px)": {
-                  height: "25px",
-                  width: "25px",
-                },
-                " @media(max-width:479px)": {
-                  height: "25px",
-                  width: "25px",
-                },
-              }}
-            /> */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="28"
@@ -276,7 +139,7 @@ function Message({ message, isJSONString, dragRef }) {
               height: "fit-content",
               minWidth: "250px",
               borderRadius: "10px 10px 10px 1px",
-              maxWidth: "90%",
+              width: "100%",
             }}
           >
             {message?.wait ? (
