2c72c1638564072699c7bd1ba8cbe22fe3458a9f
parakhjain23 <parakhjain2301@gmail.com>
2024-10-14 14:44:49 +0530

    optimize messagelist and fix scroll to bottom icon

--- src/pages/interface/components/Interface-Chatbot/MessageList.tsx
+++ src/pages/interface/components/Interface-Chatbot/MessageList.tsx
@@ -1,10 +1,17 @@
-import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
-import { Box, IconButton } from "@mui/material";
-import React, { useContext, useEffect, useRef, useState } from "react";
-import "./InterfaceChatbot.scss";
-import Message from "./Message.tsx";
-import { MessageContext } from "./InterfaceChatbot.tsx";
+import { Box } from "@mui/material";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { sendFeedbackAction } from "../../../../api/InterfaceApis/InterfaceApis.ts";
+import "./InterfaceChatbot.scss";
+import { MessageContext } from "./InterfaceChatbot.tsx";
+import Message from "./Message.tsx";
+import MoveToDownButton from "./MoveToDownButton.tsx";
 
 function MessageList() {
   const containerRef = useRef<any>(null);
@@ -13,50 +20,53 @@ function MessageList() {
   const [showScrollButton, setShowScrollButton] = useState(false); // State to control the visibility of the button
   const [shouldScroll, setShouldScroll] = useState(true);
 
-  const handleFeedback = async (
-    messageId: string,
-    feedbackStatus: number,
-    currentStatus: number
-  ) => {
-    if (messageId && feedbackStatus && currentStatus !== feedbackStatus) {
-      setShouldScroll(false);
-      const response: any = await sendFeedbackAction({
-        messageId,
-        feedbackStatus,
-      });
-      if (response?.success) {
-        const messageId = response?.result?.[0]?.message_id;
-        // iterate over messages and update the feedback status of the message whosse rolw is assistant
-        for (let i = messages?.length - 1; i >= 0; i--) {
-          const message = messages[i];
-          if (
-            message?.role === "assistant" &&
-            message?.message_id === messageId
-          ) {
-            message.user_feedback = feedbackStatus;
-            break; // Assuming only one message needs to be updated
+  const handleFeedback = useCallback(
+    async (
+      messageId: string,
+      feedbackStatus: number,
+      currentStatus: number
+    ) => {
+      if (messageId && feedbackStatus && currentStatus !== feedbackStatus) {
+        setShouldScroll(false);
+        const response: any = await sendFeedbackAction({
+          messageId,
+          feedbackStatus,
+        });
+        if (response?.success) {
+          const messageId = response?.result?.[0]?.message_id;
+          // Iterate over messages and update the feedback status of the message whose role is assistant
+          for (let i = messages?.length - 1; i >= 0; i--) {
+            const message = messages[i];
+            if (
+              message?.role === "assistant" &&
+              message?.message_id === messageId
+            ) {
+              message.user_feedback = feedbackStatus;
+              break; // Assuming only one message needs to be updated
+            }
           }
+          setMessages([...messages]);
         }
-        setMessages([...messages]);
       }
-    }
-  };
+    },
+    [messages, setMessages, sendFeedbackAction]
+  );
 
-  const movetoDown = () => {
+  const movetoDown = useCallback(() => {
     containerRef.current?.scrollTo({
       top: containerRef?.current?.scrollHeight,
       behavior: "smooth",
     });
-  };
+  }, []);
 
-  const handleScroll = () => {
+  const handleScroll = useCallback(() => {
     const currentContainer = containerRef.current;
     const scrollPosition = currentContainer.scrollTop;
     const maxScrollTop =
       currentContainer.scrollHeight - currentContainer.clientHeight;
 
     // Show the button if scrolled up
-    if (scrollPosition < maxScrollTop) {
+    if (scrollPosition < maxScrollTop - 150) {
       setShowScrollButton(true);
     }
 
@@ -64,15 +74,14 @@ function MessageList() {
     if (scrollPosition >= maxScrollTop - 10) {
       setShowScrollButton(false);
     }
-  };
+  }, []);
 
   useEffect(() => {
     if (shouldScroll) {
       movetoDown();
     }
-    // Re-enable scrolling after the effect runs
     setShouldScroll(true);
-  }, [messages]);
+  }, [messages, movetoDown]);
 
   useEffect(() => {
     const currentContainer = containerRef.current;
@@ -80,7 +89,17 @@ function MessageList() {
     return () => {
       currentContainer?.removeEventListener("scroll", handleScroll); // Clean up scroll listener
     };
-  }, []);
+  }, [handleScroll]);
+
+  const RenderMessages = useMemo(() => {
+    return messages?.map((message, index) => (
+      <Message
+        key={message.message_id || index} // Use a unique identifier if available
+        message={message}
+        handleFeedback={handleFeedback}
+      />
+    ));
+  }, [messages, handleFeedback]); // Include handleFeedback in dependencies
 
   return (
     <Box
@@ -90,37 +109,17 @@ function MessageList() {
         display: "flex",
         flexDirection: "column",
         padding: 2,
-        // backgroundColor: theme.palette.primary.main,
       }}
       ref={containerRef}
     >
       <Box sx={{ flex: "1 1 auto", minHeight: 0 }}>
-        {messages?.map((message, index) => (
-          <Message
-            key={index}
-            message={message}
-            handleFeedback={handleFeedback}
-          />
-        ))}
+        {RenderMessages} {/* Corrected to render the component */}
       </Box>
-      {showScrollButton && messages?.length > 5 && (
-        <IconButton
-          onClick={movetoDown}
-          className="move-to-down-button"
-          sx={{
-            backgroundColor: "#333",
-            color: "white",
-            position: "fixed",
-            bottom: "20px",
-            right: "20px",
-          }}
-          disableRipple
-        >
-          <KeyboardDoubleArrowDownIcon color="inherit" />
-        </IconButton>
-      )}
+      <MoveToDownButton
+        movetoDown={movetoDown}
+        showScrollButton={showScrollButton}
+      />
     </Box>
   );
 }
-
 export default MessageList;
