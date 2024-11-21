f314289f13f745e8b2ba0437dfa3f9c6dacb252c
parakhjain23 <parakhjain2301@gmail.com>
2024-11-21 12:39:58 +0530

    extract header tab

--- src/pages/interface/components/Interface-Chatbot/InterfaceChatbot.tsx
+++ src/pages/interface/components/Interface-Chatbot/InterfaceChatbot.tsx
@@ -17,24 +17,22 @@ import {
   sendDataToAction,
 } from "../../../../api/InterfaceApis/InterfaceApis.ts";
 import { errorToast } from "../../../../components/customToast.js";
+import FormComponent from "../../../../components/FormComponent.js";
 import { ParamsEnums } from "../../../../enums";
 import addUrlDataHoc from "../../../../hoc/addUrlDataHoc.tsx";
 import {
   getHelloDetailsStart,
   setChannel,
-  setHuman,
 } from "../../../../store/hello/helloSlice.ts";
 import { $ReduxCoreType } from "../../../../types/reduxCore.ts";
 import { useCustomSelector } from "../../../../utils/deepCheckSelector.js";
 import useSocket from "../../hooks/socket.js";
+import { GetSessionStorageData } from "../../utils/InterfaceUtils.ts";
 import ChatbotHeader from "./ChatbotHeader.tsx";
+import ChatbotHeaderTab from "./ChatbotHeaderTab.tsx";
 import ChatbotTextField from "./ChatbotTextField.tsx";
 import "./InterfaceChatbot.scss";
 import MessageList from "./MessageList.tsx";
-import { GetSessionStorageData } from "../../utils/InterfaceUtils.ts";
-import FormComponent from "../../../../components/FormComponent.js";
-import { Tab, Tabs } from "@mui/material";
-import { AiIcon, UserAssistant } from "../../../../assests/assestsIndex.ts";
 
 const client = new WebSocketClient(
   "lyvSfW7uPPolwax0BHMC",
@@ -128,16 +126,6 @@ function InterfaceChatbot({
   const [helloId, setHelloId] = useState(
     GetSessionStorageData("helloId") || reduxHelloId
   );
-
-  const [value, setValue] = useState(IsHuman ? "Human" : "AI"); // Set default tab based on IsHuman
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    if (newValue == "Human") dispatch(setHuman({}));
-    else dispatch(setHuman({ isHuman: false }));
-    setValue(newValue);
-  };
-
-  const isHelloAssistantEnabled = mode?.length > 0;
 
   useEffect(() => {
     setThreadId(GetSessionStorageData("threadId"));
@@ -541,62 +529,7 @@ function InterfaceChatbot({
         }}
       >
         <ChatbotHeader setChatsLoading={setChatsLoading} />
-        {isHelloAssistantEnabled && (
-          <Tabs
-            value={value}
-            onChange={(e, newValue) => handleChange(e, newValue)}
-            centered
-            sx={{ minHeight: "2px", padding: "0" }}
-          >
-            <Tab
-              value={"AI"}
-              label="AI"
-              icon={
-                <img
-                  src={AiIcon}
-                  width="24"
-                  height="24"
-                  alt="AI Icon"
-                  style={{
-                    marginRight: 4,
-                    filter: "drop-shadow(0 0 5px pink)",
-                  }}
-                />
-              }
-              iconPosition="start"
-              sx={{
-                fontSize: "0.8rem",
-                padding: "0 2px",
-                color: "black",
-                minHeight: "40px",
-              }}
-            />
-            <Tab
-              value={"Human"}
-              label="Human"
-              icon={
-                <img
-                  src={UserAssistant}
-                  width="24"
-                  height="24"
-                  alt="Human Icon"
-                  className="icon-visible"
-                  style={{
-                    cursor: "pointer",
-                    filter: !IsHuman ? "drop-shadow(0 0 5px pink)" : "",
-                  }}
-                />
-              }
-              iconPosition="start"
-              sx={{
-                fontSize: "0.8rem",
-                padding: "0 2px",
-                color: "black",
-                minHeight: "40px",
-              }}
-            />
-          </Tabs>
-        )}
+        <ChatbotHeaderTab />
         {chatsLoading && (
           <LinearProgress
             variant="indeterminate"
