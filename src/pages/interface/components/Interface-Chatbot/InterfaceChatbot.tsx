bc99d6bfebf6a3daa74fc10821ac3d197f5543d9
parakhjain23 <parakhjain2301@gmail.com>
2024-11-26 17:41:47 +0530

    suggestion in chatbot

--- src/pages/interface/components/Interface-Chatbot/InterfaceChatbot.tsx
+++ src/pages/interface/components/Interface-Chatbot/InterfaceChatbot.tsx
@@ -436,6 +436,8 @@ function InterfaceChatbot({
               content: "Resetting the chat",
             },
           ]);
+        } else if (parsedMessage?.response?.data?.suggestions !== undefined) {
+          setOptions(parsedMessage.response?.data?.suggestions || []);
         } else if (parsedMessage?.response?.data) {
           // Handle the new structure with response data
           // const content = parsedMessage.response.data.content;
@@ -447,7 +449,6 @@ function InterfaceChatbot({
               ...(parsedMessage.response.data || {}),
             },
           ]);
-          setOptions(parsedMessage.response?.options || []);
           clearTimeout(timeoutIdRef.current);
         } else {
           // Handle any other cases
